acdb83d1374bf5562aee543e30c74aed7c37988d
VtoCorleone <shane.chapman@live.com>
2015-11-06 13:58:55 -0800

    Put some more controls on the board

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,10 +9,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider!
+    
+    var currentValue: Int = 0
+    var targetValue: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        currentValue = lroundf(slider.value)
+        // generate number from 1 - 99 (100 is exclusive)
+        startNewRound()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,19 +28,33 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
+        let message = "The value of the slider is: \(currentValue) \nThe target value is: \(targetValue)"
+
         // alert message
         let alert = UIAlertController(title: "Hello, World",
-            message: "This is my first app!",
+            message: message,
             preferredStyle: .Alert)
         
         // alert button
-        let action = UIAlertAction(title: "Awesome",
+        let action = UIAlertAction(title: "OK",
             style: .Default,
             handler: nil)
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
+        
+        startNewRound()
+    }
+    
+    @IBAction func sliderMoved(slider: UISlider) {
+        currentValue = lroundf(slider.value)
+    }
+    
+    func startNewRound() {
+        targetValue = 1 + Int(arc4random_uniform(100))
+        currentValue = 50
+        slider.value = Float(currentValue)
     }
 }
 
