86ae873b7a30093be32734b16ac86ed2aa54b247
VtoCorleone <shane.chapman@live.com>
2015-11-09 09:22:08 -0800

    Put the final touches on the sample app

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -7,19 +7,44 @@
 //
 
 import UIKit
+import QuartzCore
 
 class ViewController: UIViewController {
     
     @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var targetLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet weak var roundLabel: UILabel!
     
-    var currentValue: Int = 0
-    var targetValue: Int = 0
+    var currentValue = 0
+    var targetValue = 0
+    var score = 0
+    var round = 0
     
     override func viewDidLoad() {
+        let thumbImageNormal = UIImage(named: "SliderThumb-Normal")
+        slider.setThumbImage(thumbImageNormal, forState: .Normal)
+        
+        let thumbImageHighlighted = UIImage(named: "SliderThumb-Highlighted")
+        slider.setThumbImage(thumbImageHighlighted, forState: .Highlighted)
+        
+        let insets = UIEdgeInsets(top: 0, left: 14, bottom: 0, right: 14)
+        
+        if let trackLeftImage = UIImage(named: "SliderTrackLeft") {
+            let trackLeftResizable = trackLeftImage.resizableImageWithCapInsets(insets)
+            slider.setMinimumTrackImage(trackLeftResizable, forState: .Normal)
+        }
+        
+        if let trackRightImage = UIImage(named: "SliderTrackRight") {
+            let trackRightResizable = trackRightImage.resizableImageWithCapInsets(insets)
+            slider.setMaximumTrackImage(trackRightResizable, forState: .Normal)
+        }
+        
         super.viewDidLoad()
-        currentValue = lroundf(slider.value)
         // generate number from 1 - 99 (100 is exclusive)
-        startNewRound()
+        startNewGame()
+        updateLabels()
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,33 +53,82 @@ class ViewController: UIViewController {
     }
 
     @IBAction func showAlert() {
-        let message = "The value of the slider is: \(currentValue) \nThe target value is: \(targetValue)"
+        let difference = abs(targetValue - currentValue)
+        var points = 100 - difference
+        
+        let title: String
+        if difference == 0 {
+            title = "Perfect!"
+            points += 100
+        } else if difference < 5 {
+            title = "You almost had it!"
+            if difference == 1 {
+                points += 50
+            }
+        } else if difference < 10 {
+            title = "Pretty good!"
+        } else {
+            title = "Not even close..."
+        }
+        
+        score += points
+        
+        let message = "You scored \(points)"
 
         // alert message
-        let alert = UIAlertController(title: "Hello, World",
+        let alert = UIAlertController(title: title,
             message: message,
             preferredStyle: .Alert)
         
         // alert button
         let action = UIAlertAction(title: "OK",
             style: .Default,
-            handler: nil)
+            handler: {
+                action in
+                self.startNewRound()
+                self.updateLabels()
+            })
         
         alert.addAction(action)
         
         presentViewController(alert, animated: true, completion: nil)
         
-        startNewRound()
     }
     
     @IBAction func sliderMoved(slider: UISlider) {
         currentValue = lroundf(slider.value)
     }
     
+    @IBAction func startOver() {
+        startNewGame()
+        updateLabels()
+            
+        let transition = CATransition()
+        transition.type = kCATransitionFade
+        transition.duration = 1
+        transition.timingFunction = CAMediaTimingFunction(name:
+        kCAMediaTimingFunctionEaseOut)
+        view.layer.addAnimation(transition, forKey: nil)
+    }
+    
+    func startNewGame() {
+        score = 0
+        round = 0
+        startNewRound()
+    }
+    
     func startNewRound() {
         targetValue = 1 + Int(arc4random_uniform(100))
         currentValue = 50
         slider.value = Float(currentValue)
+        round++
     }
+    
+    func updateLabels() {
+        targetLabel.text = String(targetValue)
+        scoreLabel.text = String(score)
+        roundLabel.text = String(round)
+    }
+    
 }
 
